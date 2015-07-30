edd4c5f3cc8a6cc3fee8a93d61efbce9fbedca63
elvece <lvcifferello@gmail.com>
2015-07-29 22:20:36 -0600

    refactored functions to make more modular and added functionality so user is asked to continue

--- main-refactor1.js
+++ main-refactor1.js
@@ -1,65 +1,55 @@
 //global variables
 var personArray = [];
 
-
 //all functions
-// function getNumOfPeople(str){
-//   return prompt("How many " + str + " are learning Javascript?");
-// }
 
-//For each student, ask the user for their name, phone number, and city. Create an object for each person. Add each object to an array.
+//For each person, asks the user for their name, phone number, and city. Creates an object for each person. Adds each object to an array.
+function getInfo(){
+  var initialInfoArray = [];
+  var name = prompt("What is your full name?");
+  var city = prompt("What city do you live in?");
+  var phoneNumber = prompt("What is your phone number?");
+  var person = {
+    City: city,
+    Name: name,
+    Phone: phoneNumber,
+  };
+  initialInfoArray.push(person);
+  return initialInfoArray;
+}
 
-function getInfo(num){
-  var infoArray = [];
-    var name = prompt("What is your full name?");
-    var city = prompt("What city do you live in?");
-    var phoneNumber = prompt("What is your phone number?");
-    var person = {
-      City: city,
-      Name: name,
-      Phone: phoneNumber,
-    };
-    infoArray.push(person);
-  }
-  return infoArray;
+//function that takes in the information from getInfo() and alerts it in a nicely formatted way; also pushes this array information to personArray
+function outputInfo (){
+  var outputInfoArray = getInfo();
+  alert("Name: " + outputInfoArray[0].Name + "\n" + "City: " + outputInfoArray[0].City + "\n" + "Phone Number: " + outputInfoArray[0].Phone);
+  personArray.push(outputInfoArray);
 }
 
 //calling functions
-alert("Enter student information"); //do you want to enter another student
-outputInfo("students");
-
-//refactor so applicable to students and mentors, make function
-
-function outputInfo (str){
-  var num = getNumOfPeople(str);
-  var infoArray = getInfo(num);
-  infoArray.push(infoArray);
-  for (var i = 0; i < num; i++) {
-    alert("Name: " + infoArray[0][i].Name + "\n" + "City: " + infoArray[0][i].City + "\n" + "Phone Number: " + infoArray[0][i].Phone);
+alert("Enter student information.");
+outputInfo();
+//while the prompt is true, prompts if wants to add more students then runs outoutInfo() if user enters yes, otherwise, ends the entering info phase through break
+while(true){
+var addStudents = prompt("Would you like to add more students? Please answer yes or no.");
+  if (addStudents === "yes"){
+    outputInfo();
   }
-  console.log(infoArray);
+  else {
+    break;
+  }
 }
 
+alert("Enter mentor information.");
+outputInfo();
+//while the prompt is true, prompts if wants to add more mentors then runs outoutInfo() if user enters yes, otherwise, ends the entering info phase through break
+while(true){
+  var addMentor = prompt("Would you like to add another Mentor? Please answer yes or no.");
+  if (addMentor === "yes"){
+    outputInfo();
+  }
+  else {
+    break;
+  }
+}
 
-
-// var numOfStudents = getNumOfPeople("students");
-// var studentArray = getInfo(numOfStudents);
-// personArray.push(studentArray);
-// for (var i = 0; i < numOfStudents; i++) {
-//   alert("Name: " + personArray[0][i].Name + "\n" + "City: " + personArray[0][i].City + "\n" + "Phone Number: " + personArray[0][i].Phone);
-// }
-// var addStudents = alert("Would you like to add more students?");
-// if (addStudents === "yes"){
-
-  //run new refactored function for students
-// }
-
-//repeat for mentors
-
-// alert("Enter mentor information");
-// var numOfMentors = getNumOfPeople("mentors");
-// var mentorArray = getInfo(numOfMentors);
-// personArray.push(mentorArray);
-
-
-// console.log(personArray);
+console.log(personArray);
